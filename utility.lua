65560fd7cc74e5eef7371bbbc9ca8243756e6d1a
aerlrobos <aerlrosefu@gmail.com>
2026-02-09 00:26:37 +0200

    Update utility.lua

--- utility.lua
+++ utility.lua
@@ -1,5 +1,30 @@
 local Module = {}
 
+function Module:Discord(code)
+    local req = (syn and syn.request) or (http and http.request) or http_request
+    pcall(function()
+        if req then
+            req({
+                Url = 'http://127.0.0.1:6463/rpc?v=1',
+                Method = 'POST',
+                Headers = {
+                    ['Content-Type'] = 'application/json',
+                    Origin = 'https://discord.com'
+                },
+                Body = game:GetService('HttpService'):JSONEncode({
+                    cmd = 'INVITE_BROWSER',
+                    nonce = game:GetService('HttpService'):GenerateGUID(false),
+                    args = {code = code}
+                })
+            })
+        else
+            if setclipboard then
+                setclipboard(tostring(code))
+            end
+        end
+    end)
+end
+
 function Module:TapSimulatorRemoteBypass()
     local tables = {}
     for _, obj in pairs(getgc(true)) do
